b50fcac52077653e3b25489a26d87e841e1215db
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2022-07-09 13:17:16 +0200

    Update PipeBlock.java

--- src/main/java/de/maxhenkel/pipez/blocks/PipeBlock.java
+++ src/main/java/de/maxhenkel/pipez/blocks/PipeBlock.java
@@ -97,7 +97,7 @@ public abstract class PipeBlock extends Block implements IItemBlock, SimpleWater
     }
 
     public InteractionResult onWrenchClicked(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit, Direction side) {
-        if (!player.isShiftKeyDown()) {
+        if (player.isShiftKeyDown()) {
             return InteractionResult.PASS;
         }
         if (side != null) {
